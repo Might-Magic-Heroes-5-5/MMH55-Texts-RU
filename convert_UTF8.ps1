6f856d57a9cfee78bce2e6781ba3351b87236115
dredknight <gangstarrr@abv.bg>
2025-04-12 17:00:51 +0300

    skillwheel fixes

--- convert_UTF8.ps1
+++ convert_UTF8.ps1
@@ -1,15 +1,19 @@
-$directory = "$PSScriptRoot\mod\"
+$directory = "$PSScriptRoot\MMH55-Texts-RU\"
 Write-Output "Processing directory..."
 
 # - UTF-8 is used by this repository so github can show diff during PRs
 $encoding = [System.Text.Encoding]::UTF8
 
-Get-ChildItem -Path $directory -Filter "*.txt" -Recurse | ForEach-Object {
-	$inputFile = $_.FullName
-	Write-Output "Converting: $inputFile"
+Get-ChildItem -Path $directory -Recurse -Filter "*.bak" | Remove-Item -Force
 
-	## Read the file and write it back in UTF-8
-	$content = Get-Content -Raw -Path $inputFile
-	[System.IO.File]::WriteAllText($inputFile, $content, $encoding)
+Get-ChildItem -Path $directory -Filter "*.txt" -Recurse | ForEach-Object {
+    $inputFile = $_.FullName
+    Write-Output "Converting: $inputFile"
+
+    if ($_.IsReadOnly) { $_.IsReadOnly = $false }
+
+    ## Read the file and write it back in UTF-8
+    $content = Get-Content -Raw -Path $inputFile
+    [System.IO.File]::WriteAllText($inputFile, $content, $encoding)
 }
 Write-Output "Conversion complete!"
